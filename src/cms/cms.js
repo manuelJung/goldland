57e2f096f886fe6c3e3d1e4733eff3508683cb77
manuelJung <manu.jung@relaxcommerce.com>
2020-03-24 22:35:42 +0100

    setup netlify cms

---
+++ src/cms/cms.js
@@ -0,0 +1,10 @@
+import CMS from 'netlify-cms-app'
+// import HomeRoute from './preview/HomeRoute'
+// import LanguageString from './widgets/LanguageString'
+
+// CMS.registerPreviewTemplate('homepage-de', HomeRoute)
+// CMS.registerPreviewTemplate('homepage-en', HomeRoute)
+// CMS.registerPreviewTemplate('homepage-mk', HomeRoute)
+// CMS.registerPreviewTemplate('homepage-al', HomeRoute)
+
+// CMS.registerWidget('language-string', LanguageString)
