7aa53503bea6dba0203d486853aecffa3b560256
manuelJung <manu.jung@relaxcommerce.com>
2020-03-29 23:28:16 +0200

    setup transformer

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/cms/templates/pages`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,3 +1,4 @@
+const fs = require('fs')
 /**
  * Implement Gatsby's Node APIs in this file.
  *
@@ -5,3 +6,31 @@
  */
 
 // You can delete this file if you're not using it
+
+const locales = ['de', 'en', 'mk', 'al']
+
+export async function onCreateNode ({ node, actions, createNodeId }) {
+  if(node.sourceInstanceName === 'pages'){
+    const rawPage = require(node.absolutePath)
+    locales.forEach(locale => {
+      let page = {}
+      for(let key in rawPage) {
+        if(typeof rawPage[key] !== 'object') page[key] = rawPage[key]
+        else if(!rawPage[key].__localized) page[key] = rawPage[key]
+        else page[key] = rawPage[key][locale] || ''
+      }
+
+      actions.createNode({
+        ...page,
+        _locale: locale,
+        id: createNodeId(`Page-${node.id}-${locale}`),
+        parent: node.id,
+        internal: {
+          type: 'Page',
+          contentDigest: node.internal.contentDigest,
+          mediaType: `application/json`
+        }
+      })
+    })
+  }
+}
